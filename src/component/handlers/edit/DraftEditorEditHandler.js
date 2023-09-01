1e499de03e49393cff2efd69d7fe3975c5f3ed1c
Martin Hejtmanek <martinh@kontent.ai>
2023-09-01 15:58:30 +0200

    KCL-11106 - Update selection before handling input in case the selection update via mouse click/keyboard navigation hasn't finished yet

--- src/component/handlers/edit/DraftEditorEditHandler.js
+++ src/component/handlers/edit/DraftEditorEditHandler.js
@@ -14,6 +14,7 @@
 import type DraftEditor from 'DraftEditor.react';
 
 const UserAgent = require('UserAgent');
+const Keys = require('Keys');
 
 const onBeforeInput = require('editOnBeforeInput');
 const onBlur = require('editOnBlur');
@@ -31,11 +32,52 @@ const onSelect = require('editOnSelect');
 const isChrome = UserAgent.isBrowser('Chrome');
 const isFirefox = UserAgent.isBrowser('Firefox');
 
-const selectionHandler: (e: DraftEditor) => void =
-  isChrome || isFirefox ? onSelect : e => {};
+// In certain cases, contenteditable on chrome does not fire the onSelect
+// event, causing problems with cursor positioning. Therefore, the selection
+// state update handler is added to more events to ensure that the selection
+// state is always synced with the current cursor positions.
+const explicitlyHandleCaretPositioning = isChrome || isFirefox;
+
+const keysDown: Set<number> = new Set();
+
+function markTrackedKeyDown(e: SyntheticKeyboardEvent<HTMLElement>) {
+  const keyCode = e.which;
+  switch (keyCode) {
+    // We are tracking only keys that may change selection without changing content
+    case Keys.UP:
+    case Keys.RIGHT:
+    case Keys.DOWN:
+    case Keys.LEFT:
+    case Keys.PAGE_UP:
+    case Keys.PAGE_DOWN:
+    case Keys.HOME:
+    case Keys.END:
+      keysDown.add(keyCode);
+      break;
+  }
+}
+
+function markKeyUp(e: SyntheticKeyboardEvent<HTMLElement>) {
+  keysDown.delete(e.which);
+}
+
+let mouseButtonsDown = 0;
+
+function updateMouseButtons(editor: DraftEditor, e: SyntheticMouseEvent) {
+  mouseButtonsDown = e.buttons;
+}
 
 const DraftEditorEditHandler = {
-  onBeforeInput,
+  onBeforeInput: explicitlyHandleCaretPositioning
+    ? (editor, e) => {
+        // Selection event may not fire if one starts typing before mouse/key up, so in this case we need to update the selection first
+        // otherwise the editor content could be malformed and handleBeforeInput could get incorrect selection
+        if (mouseButtonsDown || keysDown.size) {
+          onSelect(editor);
+        }
+        onBeforeInput(editor, e);
+      }
+    : onBeforeInput,
   onBlur,
   onCompositionStart,
   onCopy,
@@ -44,15 +86,30 @@ const DraftEditorEditHandler = {
   onDragStart,
   onFocus,
   onInput,
-  onKeyDown,
   onPaste,
   onSelect,
-  // In certain cases, contenteditable on chrome does not fire the onSelect
-  // event, causing problems with cursor positioning. Therefore, the selection
-  // state update handler is added to more events to ensure that the selection
-  // state is always synced with the actual cursor positions.
-  onMouseUp: selectionHandler,
-  onKeyUp: selectionHandler,
+
+  onMouseDown: explicitlyHandleCaretPositioning
+    ? updateMouseButtons
+    : undefined,
+  onMouseUp: explicitlyHandleCaretPositioning
+    ? (editor: DraftEditor, e: SyntheticMouseEvent) => {
+        updateMouseButtons(editor, e);
+        onSelect(editor);
+      }
+    : undefined,
+  onKeyDown: explicitlyHandleCaretPositioning
+    ? (editor: DraftEditor, e: SyntheticKeyboardEvent<HTMLElement>) => {
+        markTrackedKeyDown(e);
+        onKeyDown(editor, e);
+      }
+    : onKeyDown,
+  onKeyUp: explicitlyHandleCaretPositioning
+    ? (editor: DraftEditor, e: SyntheticKeyboardEvent<HTMLElement>) => {
+        markKeyUp(e);
+        onSelect(editor);
+      }
+    : undefined,
 };
 
 module.exports = DraftEditorEditHandler;
